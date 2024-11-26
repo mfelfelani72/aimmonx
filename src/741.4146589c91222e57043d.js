4728e7fc9a389931444545f705b306024927ae54
mohammad <mfelfelani72@gmail.com>
2024-11-26 16:52:19 +0330

    edit

--- src/741.4146589c91222e57043d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbuiler=self.webpackChunkbuiler||[]).push([[741],{4741:(e,n,t)=>{t.r(n),t.d(n,{default:()=>l});var u=t(6540),a=(0,u.lazy)((function(){return t.e(527).then(t.bind(t,4527))}));const l=function(){return u.createElement("div",{className:"!font-yekanBakh"},u.createElement(a,null))}}}]);
